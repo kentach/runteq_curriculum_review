1641f35e7b4b024c233c0ca32dde02ac48811e9b
Kenta <kenta@Kentas-MacBook-Pro.local>
2026-04-22 19:22:43 +0900

    03__i18n

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   root 'static_pages#top'
+
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
+  resources :posts
 end
