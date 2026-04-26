5c959401f0ac2a5f3545f47781117113071b3ba9
Kenta <kenta@Kentas-MacBook-Pro.local>
2026-04-26 15:43:05 +0900

    画像の投稿機能

--- app/models/board.rb
+++ app/models/board.rb
@@ -3,4 +3,5 @@ class Board < ApplicationRecord
 
   validates :title, presence: true, length: { maximum: 255 }
   validates :body, presence: true, length: { maximum: 65_535 }
+  mount_uploader :board_image, BoardImageUploader
 end
