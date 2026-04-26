9b7f98b8542d67d6fdd8cd6d8d21f3f61073d679
Kenta <kenta@Kentas-MacBook-Pro.local>
2026-04-26 17:26:15 +0900

    タイトルを動的にする

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,22 @@
+class CommentsController < ApplicationController
+  def create
+    @comment = current_user.comments.build(comment_params)
+    if @comment.save
+      redirect_to board_path(@comment.board), success: t('defaults.flash_message.created', item: Comment.model_name.human)
+    else
+      redirect_to board_path(@comment.board), danger: t('defaults.flash_message.not_created', item: Comment.model_name.human)
+    end
+  end
+
+  def destroy
+    @comment = current_user.comments.find(params[:id])
+    @comment.destroy
+    redirect_to board_path(@comment.board), success: t('defaults.flash_message.destroyed', item: Comment.model_name.human)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body).merge(board_id: params[:board_id])
+  end
+end
